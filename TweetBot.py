029ce25ca96965046bbaf0c4766ed0138204b764
jangui <jaimitino@gmail.com>
2020-04-25 15:33:55 -0400

    added keys example file

--- TweetBot.py
+++ TweetBot.py
@@ -2,17 +2,28 @@
 import tweepy
 
 class TweetBot():
-    def __init__(self, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET):
-        self.CONSUMER_KEY = CONSUMER_KEY
-        self.CONSUMER_SECRET = CONSUMER_SECRET
-        self.ACCESS_KEY = ACCESS_KEY
-        self.ACCESS_SECRET = ACCESS_SECRET
+    def __init__(self, keysFile):
+        self.getKeys(keysFile)
         self.auth()
 
+    def getKeys(self, filename):
+        self.keys = {}
+        with open(filename, 'r') as f:
+            lines = [line.rstrip().split(" ") for line in f]
+            for line in lines:
+                self.keys[line[0][:-1]] = line[1]
+
     def auth(self):
-        self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-        self.api = tweepy.API(self.auth)
+        callback_url = ''
+        auth = tweepy.OAuthHandler(self.keys["CONSUMER_KEY"],
+                                   self.keys["CONSUMER_SECRET"],
+                                   callback_url)
+        auth.set_access_token(self.keys["ACCESS_KEY"], self.keys["ACCESS_SECRET"])
+
+        self.api = tweepy.API(auth)
 
     def tweet(self, message):
         self.api.update_status(message)
+
+
+t = TweetBot("keys")
