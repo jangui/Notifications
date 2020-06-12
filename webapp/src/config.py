bb1a282595d69a467c0f14e557224c1e1d943b61
jangui <jaimitino@gmail.com>
2020-06-12 00:37:12 -0400

    made funcs for dealing with db

--- webapp/src/config.py
+++ webapp/src/config.py
@@ -5,6 +5,7 @@ class Config:
 
     # sqlalchemy
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@127.0.0.1:3306/notifications'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     def __init__(self):
         if os.environ.get('DEBUG', None):
--- webapp/src/models.py
+++ webapp/src/models.py
@@ -1,21 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config())
-db = SQLAlchemy(app)
+from app import db
 
 class Birthday(db.Model):
-    def __init__(**kwargs):
+    def __init__(self, **kwargs):
         super(Birthday, self).__init__(**kwargs)
 
-    __tablename__ = 'birthday'
+    __tablename__ = 'birthdays'
     first_name = db.Column(db.String(15), index=True, nullable=False, primary_key=True)
     last_name = db.Column(db.String(40), index=True, default='', primary_key= True)
     day = db.Column(db.Integer, nullable=False)
     month = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer)
-
-db.create_all()
