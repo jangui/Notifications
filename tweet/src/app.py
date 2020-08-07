23c5070468c6d06bd1cf687f012feb94fd35fd02
jangui <jaimitino@gmail.com>
2020-08-07 19:41:52 +0000

    added basic token authentication for tweet api

--- tweet/src/app.py
+++ tweet/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+from os import getenv
 
 from .config import Config
 from .utils import TweetBot
@@ -7,8 +8,9 @@ from .utils import TweetBot
 app = Flask(__name__)
 app.config.from_object(Config())
 
+MAX_TWEET = int(getenv("MAX_TWEET"))
+TOKEN = getenv("TOKEN")
 KEYFILE = "/keys"
-MAX_TWEET = 280
 
 def tweet(twt):
     if len(twt) > MAX_TWEET:
@@ -31,7 +33,7 @@ def tweet(twt):
     ret = bot.tweet(twt)
     return ret
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def tweetAPI():
     """
     function for tweeting
@@ -42,26 +44,42 @@ def tweetAPI():
 
     """
     payload = request.json
-    if (payload['tweet']) == None:
+    if payload == None or 'token' not in payload.keys():
+        return "404 page not found" #purposely unambiguous
+    if payload['token'] != TOKEN:
+        return "invalid token"
+
+    if payload['tweet'] == None:
         return "error: invalid payload, no tweet specified"
+
     ret = tweet(payload['tweet'])
     if ret[0] == False:
         return ret[1]
     return "success"
 
-@app.route('/ping', methods=['GET'])
+@app.route('/ping', methods=['GET', 'POST'])
 def ping():
     """
     hitting this endpoint will test tweet functionabilty
     """
+    if payload == None or 'token' not in payload.keys():
+        return "404 page not found" #purposely unambiguous
+    if payload['token'] != TOKEN:
+        return "invalid token"
+
     time = str(datetime.now())
     ret = tweet("PING " + time)
     if ret[0] == False:
         return ret[1]
     return "success"
 
-@app.route('/delete', methods=['POST'])
+@app.route('/delete', methods=['GET', 'POST'])
 def delete():
+    if payload == None or 'token' not in payload.keys():
+        return "404 page not found" #purposely unambiguous
+    if payload['token'] != TOKEN:
+        return "invalid token"
+
     bot = TweetBot()
 
     #get api keys from file
